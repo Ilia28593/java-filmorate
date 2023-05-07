c69868b940b0d2633b19947683f1f50ebd73028e
Ilia28593 <111430909+Ilia28593@users.noreply.github.com>
2023-05-07 22:15:25 +0300

    final version 1.2

--- src/main/java/ru/yandex/practicum/filmorate/Constance.java
+++ src/main/java/ru/yandex/practicum/filmorate/Constance.java
@@ -5,5 +5,4 @@ import java.time.LocalDate;
 public class Constance {
     public static final LocalDate FILM_BIRTHDAY_FIRST = LocalDate.of(1895, 12, 28);
     public static final int MAX_SIZE_DESCRIPTION = 200;
-    public static final int MAX_SIZE_FAVORITE_FILM = 10;
 }
