1641ec50594114c2a74239126e80701b77935134
Ilia28593 <111430909+Ilia28593@users.noreply.github.com>
2023-05-30 16:56:08 +0300

    version DB  1.1.9

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -32,7 +32,7 @@ public class FilmDbStorage implements FilmStorage {
     @Override
     public List<Film> findAll() {
         List<Film> films = new ArrayList<>();
-        SqlRowSet rs = jdbcTemplate.queryForRowSet("select * from films");
+        SqlRowSet rs = jdbcTemplate.queryForRowSet("select * from films";);
         while (rs.next()) {
             Film newFilm = new Film(rs.getString("name").trim(), rs.getString("description").trim(), rs.getDate("releaseDate").toLocalDate(), rs.getInt("duration"));
             newFilm.setId(rs.getInt("id"));
@@ -49,15 +49,13 @@ public class FilmDbStorage implements FilmStorage {
         id.getAndIncrement();
         film.setId(Long.parseLong(String.valueOf(id)));
 
-        String sqlQuery = "insert into films(id, name, description, releaseDate, duration, rate,mpa) values (?, ?, ?, ?, ?, ?, ?)";
+        String sqlQuery = "insert into films(id, name, description, releaseDate, duration, rate,mpa) values (?, ?, ?, ?, ?, ?, ?);";
         jdbcTemplate.update(sqlQuery, film.getId(), film.getName(), film.getDescription(), film.getReleaseDate(), film.getDuration(), film.getRate(), film.getMpa().getId());
 
         List<Genre> genreList = film.getGenres();
         if (genreList != null) {
             for (Genre genreId : genreList) {
-                String sqlQuery2 = """
-                        insert into films_genres (FILM_ID, GENRE_ID)
-                        values (?,?)""";
+                String sqlQuery2 = "insert into films_genres (FILM_ID, GENRE_ID);";
                 jdbcTemplate.update(sqlQuery2, film.getId(), genreId.getId());
             }
         }
@@ -69,11 +67,11 @@ public class FilmDbStorage implements FilmStorage {
     public Film filmUpdate(Film film) {
         String sqlQuery = "update films set name = ?, description = ?, releaseDate = ?, duration = ?, rate = ?, mpa = ? where id = ?";
         jdbcTemplate.update(sqlQuery, film.getName(), film.getDescription(), film.getReleaseDate(), film.getDuration(), film.getRate(), film.getMpa().getId(), film.getId());
-        String sqlQueryDel = "delete from films_genres where FILM_ID = ?";
+        String sqlQueryDel = "delete from films_genres where FILM_ID = ?;";
         jdbcTemplate.update(sqlQueryDel, film.getId());
         if (film.getGenres() != null) {
             for (Genre genreId : film.getGenres()) {
-                String sqlQuery2 = "merge into films_genres key (GENRE_ID) values (?,?)";
+                String sqlQuery2 = "merge into films_genres key (GENRE_ID) values (?,?);";
                 jdbcTemplate.update(sqlQuery2, film.getId(), genreId.getId());
             }
         }
@@ -82,7 +80,7 @@ public class FilmDbStorage implements FilmStorage {
 
     @Override
     public Film filmById(Long id) {
-        SqlRowSet rs = jdbcTemplate.queryForRowSet("select * from films where id = ?", id);
+        SqlRowSet rs = jdbcTemplate.queryForRowSet("select * from films where id = ?;", id);
         if (rs.next()) {
             Film newFilm = new Film(rs.getString("name").trim(), rs.getString("description").trim(), rs.getDate("releaseDate").toLocalDate(), rs.getInt("duration"));
             newFilm.setId(rs.getInt("id"));
@@ -97,7 +95,7 @@ public class FilmDbStorage implements FilmStorage {
     public List<Genre> setGenres(Long id) {
 
         List<Genre> listGenres = new ArrayList<>();
-        String sqlQuery2 = "select film_id, genre_id, genres_name from films_genres left join genres on films_genres.genre_id = genres.id where film_id = ?";
+        String sqlQuery2 = "select film_id, genre_id, genres_name from films_genres left join genres on films_genres.genre_id = genres.id where film_id = ?;";
         SqlRowSet rs2 = jdbcTemplate.queryForRowSet(sqlQuery2, id);
 
         while (rs2.next()) {
