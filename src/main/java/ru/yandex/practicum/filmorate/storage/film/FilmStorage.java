96880fbe002e8a46b1d98e23dba3dd0598bd61cd
Ilia28593 <111430909+Ilia28593@users.noreply.github.com>
2023-06-04 17:14:37 +0300

    restructure MPA

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.List;
-import java.util.Map;
 
 @Component
 public interface FilmStorage {
